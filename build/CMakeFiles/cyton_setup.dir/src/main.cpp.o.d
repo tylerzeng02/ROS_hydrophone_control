3ff3adc0af436c4b300b50f749636046ae5ecc2a
Tyler Zeng <tzeng@fus-student-9fbb4a.fuslab.ca>
2026-07-08 11:21:15 -0400

    modified safety features for motor limits

--- build/CMakeFiles/cyton_setup.dir/src/main.cpp.o.d
+++ build/CMakeFiles/cyton_setup.dir/src/main.cpp.o.d
@@ -147,16 +147,16 @@ CMakeFiles/cyton_setup.dir/src/main.cpp.o: \
  /usr/include/c++/12/bits/locale_facets.tcc \
  /usr/include/c++/12/bits/basic_ios.tcc \
  /usr/include/c++/12/bits/ostream.tcc /usr/include/c++/12/istream \
- /usr/include/c++/12/bits/istream.tcc \
+ /usr/include/c++/12/bits/istream.tcc /usr/include/c++/12/vector \
+ /usr/include/c++/12/bits/stl_uninitialized.h \
+ /usr/include/c++/12/bits/stl_vector.h \
+ /usr/include/c++/12/bits/stl_bvector.h \
+ /usr/include/c++/12/bits/vector.tcc \
  /home/tzeng/cyton_setup/src/dynamixel_motor.h \
  /usr/local/include/dynamixel_sdk/dynamixel_sdk.h \
  /usr/local/include/dynamixel_sdk/group_bulk_read.h \
  /usr/local/include/dynamixel_sdk/port_handler.h \
  /usr/local/include/dynamixel_sdk/packet_handler.h \
- /usr/include/c++/12/vector /usr/include/c++/12/bits/stl_uninitialized.h \
- /usr/include/c++/12/bits/stl_vector.h \
- /usr/include/c++/12/bits/stl_bvector.h \
- /usr/include/c++/12/bits/vector.tcc \
  /usr/local/include/dynamixel_sdk/group_handler.h /usr/include/c++/12/map \
  /usr/include/c++/12/bits/stl_tree.h \
  /usr/include/c++/12/ext/aligned_buffer.h \
