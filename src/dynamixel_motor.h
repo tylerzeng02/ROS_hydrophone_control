fa99a1eac8cf4477130030aabd27b3ecaf8bc53f
Tyler Zeng <tzeng@fus-student-9fbb4a.fuslab.ca>
2026-07-08 10:21:53 -0400

    updated wrapp0re for testing

--- src/dynamixel_motor.h
+++ src/dynamixel_motor.h
@@ -8,21 +8,55 @@ class DynamixelMotor
 {
 public:
     DynamixelMotor(const char* deviceName, int baudRate, float protocolVersion);
+    ~DynamixelMotor();
 
     bool connect();
     void disconnect();
 
+    bool pingMotor(int motorId);
+
+    bool enableTorque(int motorId);
+    bool disableTorque(int motorId);
+
+    bool setGoalPosition(int motorId, uint16_t position);
+    bool setMovingSpeed(int motorId, uint16_t speed);
+
     bool readPosition(int motorId, uint16_t& position);
+    bool readSpeed(int motorId, uint16_t& speed);
+    bool readLoad(int motorId, uint16_t& load);
+    bool readVoltage(int motorId, uint8_t& voltage);
+    bool readTemperature(int motorId, uint8_t& temperature);
+
+    bool moveJointRadians(int motorId, double radians);
+
+    double rawPositionToRadians(uint16_t rawPosition) const;
+    uint16_t radiansToRawPosition(double radians) const;
 
 private:
     const char* deviceName_;
     int baudRate_;
     float protocolVersion_;
+    bool connected_;
 
     dynamixel::PortHandler* portHandler_;
     dynamixel::PacketHandler* packetHandler_;
 
+    bool checkCommResult(int commResult, uint8_t dxlError, int motorId, const char* action);
+
+    static const int ADDR_TORQUE_ENABLE = 24;
+    static const int ADDR_GOAL_POSITION = 30;
+    static const int ADDR_MOVING_SPEED = 32;
     static const int ADDR_PRESENT_POSITION = 36;
+    static const int ADDR_PRESENT_SPEED = 38;
+    static const int ADDR_PRESENT_LOAD = 40;
+    static const int ADDR_PRESENT_VOLTAGE = 42;
+    static const int ADDR_PRESENT_TEMPERATURE = 43;
+
+    static const int TORQUE_ENABLE = 1;
+    static const int TORQUE_DISABLE = 0;
+
+    static const uint16_t MIN_RAW_POSITION = 0;
+    static const uint16_t MAX_RAW_POSITION = 1023;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,37 @@ int main()
         return 1;
     }
 
-    uint16_t position = 0;
     int motorId = 3;
 
+    motor.pingMotor(motorId);
+
+    uint16_t position = 0;
     if (motor.readPosition(motorId, position))
     {
-        std::cout << "Motor " << motorId << " position: " << position << std::endl;
+        std::cout << "Motor " << motorId
+                  << " raw position: " << position
+                  << std::endl;
+
+        std::cout << "Motor " << motorId
+                  << " radians: "
+                  << motor.rawPositionToRadians(position)
+                  << std::endl;
+    }
+
+    uint8_t voltage = 0;
+    if (motor.readVoltage(motorId, voltage))
+    {
+        std::cout << "Voltage: "
+                  << static_cast<int>(voltage) / 10.0
+                  << " V" << std::endl;
+    }
+
+    uint8_t temperature = 0;
+    if (motor.readTemperature(motorId, temperature))
+    {
+        std::cout << "Temperature: "
+                  << static_cast<int>(temperature)
+                  << " C" << std::endl;
     }
 
     motor.disconnect();
